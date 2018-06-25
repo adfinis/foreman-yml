6cb5cd6c9c803b4c58a21def99dc3729270378a0
Cyrillvw <cyrill.vonwattenwyl@adfinis-sygroup.ch>
2018-06-25 13:07:40 +0200

    Unlock provisioning templates before deleting them

--- foreman_yml/cleanup.py
+++ foreman_yml/cleanup.py
@@ -5,6 +5,7 @@ import log
 from base import ForemanBase
 from voluptuous import MultipleInvalid
 
+from pprint import pprint
 
 class ForemanCleanup(ForemanBase):
 
@@ -105,7 +106,13 @@ class ForemanCleanup(ForemanBase):
                 if (ptc['name'] == pt['name']):
                     pt_deleted = True
                     log.log(log.LOG_INFO, "Delete Provisioning Template '{0}'".format(pt['name']))
-
+                    ptinfo = self.fm.provisioning_templates.show(pt['name'])
+                    if ptinfo['locked']:
+                        log.log(log.LOG_INFO, "Provisioning Template '{0}' is locked, unlock first".format(pt['name']))
+                        self.fm.provisioning_templates.update(
+                            { 'locked': False },
+                            ptinfo['id']
+                        )
                     self.fm.provisioning_templates.destroy( pt['name'] )
                     continue
             if not pt_deleted:
